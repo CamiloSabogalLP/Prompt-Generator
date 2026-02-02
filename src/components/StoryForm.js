20a1833dd459048a59fd4a79c71f53f7a7e0422e
CamiloSabogalLP <camilo.beltran@localpayment.com>
2026-02-02 17:14:00 -0500

    Update options for Automatizable and Test-Type columns

--- src/components/StoryForm.js
+++ src/components/StoryForm.js
@@ -98,8 +98,8 @@ columna Status: TO DO
 columna Step Summary: el DADO QUE (la columna SOLO debe llamar Step Summary)
 columna Test Data: el CUANDO (la columna SOLO debe llamar Test Data)
 columna Expected Result: el ENTONCES (la columna SOLO debe llamar Expected Result)
-columna Automatizable: true / false (baja las dos opciones y que el usuario final elija)
-columna Test-Type:  UI - API (baja las dos opciones y que el usuario final elija)
+columna Automatizable: true o false (baja las dos opciones y que el usuario final elija)
+columna Test-Type:  UI o API (baja las dos opciones y que el usuario final elija)
 
 Nombre de la card:
 ${cardName}
